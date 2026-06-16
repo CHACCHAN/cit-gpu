d909dbc821bae7cdc65b7634653360b6d6ba8140
CHACCHAN <chacchan.info@gmail.com>
2026-06-16 16:14:27 +0900

    image generate test

---
+++ test/test.py
@@ -0,0 +1,19 @@
+import requests
+import base64
+
+url = "https://garbage-caviar-expert.ngrok-free.dev/v1/images/generations"
+data = {
+    "prompt": "very cute teddy bear",
+    "size": "1024x1024",
+    "steps": 50
+}
+
+response = requests.post(url, json=data, timeout=None)
+
+if response.status_code == 200:
+    b64_data = response.json()["data"][0]["b64_json"]
+    with open("output.png", "wb") as f:
+        f.write(base64.b64decode(b64_data))
+    print("save as output")
+else:
+    print(f"error: {response.status_code}")
